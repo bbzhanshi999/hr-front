7bff7adf1283a484f7a46bdd8f78ae5354f43b17
bbzhanshi999 <bbzhanshi999@163.com>
2019-03-11 23:20:42 +0800

    Ldines starting

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <transition appear
+                appear-active-class="animated zoomInUp"
+                name="custom-classes-transition" enter-active-class="animated zoomInUp"
+                mode="out-in">
+      <router-view/>
+    </transition>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
 
 <style>
 #app {
@@ -15,6 +22,19 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  font-size: 1rem;
+  overflow: hidden;
+}
+* {
+  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+}
+html,#app,div.container{
+  height:100% ;
+}
+body{
+  font-size: 62.5%;
+  height: 100%;
+  margin: 0;
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Index from './components/index.vue'
 import Login from '@/components/Login'
 
 Vue.use(Router)
@@ -14,19 +12,6 @@ export default new Router({
       path: '/',
       name: 'Login',
       component: Login
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
-      path:'/index',
-      name:'index',
-      component: Index
     }
   ]
 })
